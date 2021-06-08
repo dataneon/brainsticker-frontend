b65ae0fbb8665cfacd3ef6906ce9a8e8819c72d8
lousteez <louissterrett@gmail.com>
2021-06-08 15:16:28 -0500

    ShowUsers component is able to get list of users from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import ShowUsers from './components/app/ShowUsers'
 
 function App() {
   return (
     <div className="App">
       <p>App running</p>
+      <ShowUsers />
     </div>
   );
 }
